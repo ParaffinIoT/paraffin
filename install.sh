2060ad718756325bb35b708642df609d8c1da23d
Hadi Mahdavi <expandboard@users.noreply.github.com>
2019-07-07 14:09:44 +0430

    Edit colors

--- install.sh
+++ install.sh
@@ -8,19 +8,23 @@ trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
 # echo an error message before exiting
 trap 'echo "\"${last_command}\" command filed with exit code $?."' EXIT
 
+Normal='\e[0m'
+Green='\e[32m'
+Red='\e[91m'
+LightGreeen='\e[92m'
+LightYellow='\e[93m'
 
-echo "Paraffin IoT Platform is running..."
-echo ">"
+echo -e "> \e[92m Paraffin IoT Platform is running..." && echo ''
+
 
 file_paraffin="./paraffin"
 file_broker="./broker.env"
 file_apiserver="./apiserver.env"
 
-file_apiserver="./apiserver.env"
 
 if [ -f "$file_paraffin" ]
 then
-	echo "" && echo "$file_paraffin  is exist."
+	echo "" && echo -e "${LightGreen}$file_paraffin  is exist."
   exit 1
 else
 	sudo git clone https://github.com/ParaffinIoT/paraffin	
@@ -31,7 +35,7 @@ then
 	echo "" && echo "$file_broker found."
 	mv ./broker.env ./paraffin/broker.env
 else
-	echo "" && echo "$file_broker not found." && echo ">"
+	echo "" && echo -e "${LightYellow}$file_broker not found." && echo -e "${Normal}"
 	echo "Default broker.env is loading."
 fi
 
@@ -40,10 +44,10 @@ then
 	echo "" && echo "$file_apiserver found."
   	mv ./apiserver.env ./paraffin/apiserver.env
 else
-	echo "" && echo "$file_apiserver not found."
+	echo "" && echo -e "${LightYellow}$file_apiserver not found." && echo -e "${Normal}"
 	echo "Default apiserver.env is loading."
 fi
 
-cd paraffin
-echo "" && echo "Docker goes to deploy Paraffin services..."
+cd ./paraffin
+echo "" && echo -e "> ${LightGreen} Docker goes to deploy Paraffin services..." && echo -e "${Normal}>..."
 sudo docker-compose up
